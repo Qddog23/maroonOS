b9ec0c5c98a82b1570135ed9e1fca09f4255e191
Quincy D <quincy.deklerk@outlook.com>
2024-07-08 15:02:42 -0400

    Update serverStart.sh to use correct venv start

--- serverBackend/serverStart.sh
+++ serverBackend/serverStart.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # navigate to home directory, then to this directory, then execute python scripts, then back home
 
-cd /
-source home/pi/maroonOS/serverBackend/venv/bin/activate
-cd /
-cd home/pi/maroonOS/serverBackend
+cd /home/pi/maroonOS/serverBackend/venv/bin
+source activate
+cd /home/pi/maroonOS/serverBackend
 sudo python3 server.py &
 sudo python3 settingsServer.py &
 cd /

fc2971267eee81f9e65b3598546c826c3e343096
Quincy D <quincy.deklerk@outlook.com>
2024-07-08 23:39:13 -0400

    Reconfigure Chromium startup

--- frontend/chromiumStart.sh
+++ frontend/chromiumStart.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
-# start Chromium frontend
 
-checkDisplay(){
-    if [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]]; then
-        echo True
-        return 0
-    else
-        echo False
-        return 1
-    fi
-}
-
-while true; do
-    if [ "$(checkDisplay)" = True ]; then
-        startx -- -nocursor
-    fi
-    sleep 1
-done
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk 'file:///home/pi/maroonOS/frontend/sleep.html'

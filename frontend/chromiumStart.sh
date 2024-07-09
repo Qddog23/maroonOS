469aadc06509b1973d4aa7b5676c3c809cb8a1c5
Quincy D <quincy.deklerk@outlook.com>
2024-07-08 23:52:20 -0400

    Add display connection check before starting Chromium

--- frontend/chromiumStart.sh
+++ frontend/chromiumStart.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+function is_display_connected() {
+    /usr/bin/xrandr | grep -q 'HDMI-0 connected\|HDMI-1 connected'
+}
+
+while ! is_display_connected; do
+    sleep 1
+done
+
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk 'file:///home/pi/maroonOS/frontend/sleep.html'

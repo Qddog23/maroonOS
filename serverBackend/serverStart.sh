a52ebf46128454af5cf5c2c3352f61f8c32d94f7
Quincy D <quincy.deklerk@outlook.com>
2024-07-08 22:51:55 -0400

    Update serverStart.sh to use correct virtual environment path

--- serverBackend/serverStart.sh
+++ serverBackend/serverStart.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # navigate to home directory, then to this directory, then execute python scripts, then back home
 
-cd /home/pi/maroonOS/serverBackend/venv/bin
-source activate
+source /home/pi/maroonOS/serverBackend/venv/bin/activate
 cd /home/pi/maroonOS/serverBackend
 sudo python3 server.py &
 sudo python3 settingsServer.py &

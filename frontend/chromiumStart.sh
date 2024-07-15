2d2ff96cc8cd620bcd504670a2201e129f3f4dc5
Quincy D <quincy.deklerk@outlook.com>
2024-07-15 09:56:08 -0400

    Refactor chromiumStart.sh for fixed startup process

--- frontend/chromiumStart.sh
+++ frontend/chromiumStart.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-is_desktop_logged_in() {
-    pgrep gnome-session >/dev/null
-}
-
-while ! is_desktop_logged_in; do
-    sleep 5
-done
-
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk 'file:///home/pi/maroonOS/frontend/sleep.html'

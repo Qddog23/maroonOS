88d9173293e9cd5a707e944aa615f19abd99ae2a
Quincy D <quincy.deklerk@outlook.com>
2024-07-15 16:02:59 -0400

    Refactor serverStart.sh to use correct path for Python virtual environment

--- server/serverStart.sh
+++ server/serverStart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/robocubs/maroonOS/server
-sudo /home/robocubs/maroonOS/server/venv/bin/python3 server.py &
-sudo /home/robocubs/maroonOS/server/venv/bin/python3 settingsServer.py &
+sudo /home/robocubs/maroonOS/server/venv/venv/bin/python3 server.py &
+sudo /home/robocubs/maroonOS/server/venv/venv/bin/python3 settingsServer.py &
 cd /

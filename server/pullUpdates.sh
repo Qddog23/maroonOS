454879caf05e07455ca35c0e7162826254d270c0
Quincy D <quincy.deklerk@outlook.com>
2024-07-22 08:58:52 -0400

    Refactor pullUpdates.sh to simplify git reset command

--- server/pullUpdates.sh
+++ server/pullUpdates.sh
@@ -5,10 +5,7 @@ cd /
 cd home/robocubs/maroonOS
 
 git fetch
-
-if [ "$(git diff origin/main)" != "" ]; then
-    git reset --hard origin/main
-fi
+git reset --hard origin/main
 
 chmod +x server/serverStart.sh
 chmod +x server/pullUpdates.sh
